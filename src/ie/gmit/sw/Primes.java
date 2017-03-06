895394d1d1d6092075357e83f3f4ce99ebb9834b
Sean McGrath <Sean McGrath>
2017-03-06 17:01:08 +0000

    improved algorithim

--- src/ie/gmit/sw/Primes.java
+++ src/ie/gmit/sw/Primes.java
@@ -4,14 +4,24 @@ public class Primes {
 	public static void main(String[] args) {
 		//is_prime(7);
 		Primes p = new Primes();
-		System.out.println(p.isPrime(1111));
+		System.out.println(p.isPrime(137));
 	}
+	//checks whether an int prime or not
 	boolean isPrime(int n) {
-	    for(int i=2;i<n;i++) {
+	    for(int i=2; i < n; i++) {
+	    	System.out.println(i);
 	        if(n%i==0)
 	            return false;
 	    }
 	    return true;
 	}
-	
+	//in this method we multiple 2 x i 
+//	boolean isPrime(int n) {
+//	    for(int i = 2; 2*i < n; i++) {
+//	    	System.out.println(i);
+//	        if(n % i == 0)
+//	            return false;
+//	    }
+//	    return true;
+//	}
 }
